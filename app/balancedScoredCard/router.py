72b1df6fc252e7499e98401eab26e7ae2db04727
Christopher Salazar <christopher.salazar@tia.com.ec>
2023-02-26 14:03:46 -0500

    Christopher: Endpoints para objetivos

--- app/balancedScoredCard/router.py
+++ app/balancedScoredCard/router.py
@@ -4,6 +4,6 @@ from app.balancedScoredCard.views import PerspectiveViewSet, IndicatorViewSet
 router = DefaultRouter()
 router.register('/perspectiva', PerspectiveViewSet, basename='perspectiva')
 router.register('/indicadores', IndicatorViewSet, basename='indicadores')
-
+router.register('/objetivos', ObjectiveViewSet, basename='objetivos')
 
 urlpatterns = router.urls
--- app/balancedScoredCard/views.py
+++ app/balancedScoredCard/views.py
@@ -132,3 +132,64 @@ class IndicatorViewSet(Authentication, viewsets.ModelViewSet):
         except:
             return ResponseData.Response(TYPECODE.SI, TYPECODE.INTERNAL_ERROR, MESSAGE.INTERNAL_ERROR, MESSAGE.NULL, status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+class ObjectiveViewSet(Authentication, viewsets.ModelViewSet):
+    serializer_class = ObjectiveSerializer
+
+    def get_queryset(self, pk=None):
+        if pk is None:
+            return self.get_serializer().Meta.model.objects.filter(state='A')
+        return self.get_serializer().Meta.model.objects.filter(state='A', id=pk).first()
+
+    def list(self, request):
+        print(TYPECODE.SI)
+        objective_serializer = self.get_serializer(
+            self.get_queryset(), many=True)
+        return ResponseData.Response(TYPECODE.NO, TYPECODE.OK, MESSAGE.OK, objective_serializer.data, status.HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+        if self.get_queryset(pk):
+            objective_serializer = self.serializer_class(
+                self.get_queryset(pk))
+            return ResponseData.Response(TYPECODE.NO, TYPECODE.OK, MESSAGE.OK, objective_serializer.data, status.HTTP_200_OK)
+        return ResponseData.Response(TYPECODE.SI, TYPECODE.NOT_FOUND, MESSAGE.NOT_FOUND, MESSAGE.NULL, status.HTTP_404_NOT_FOUND)
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return ResponseData.Response(TYPECODE.NO, TYPECODE.CREATED, MESSAGE.CREATED, MESSAGE.NULL, status.HTTP_201_CREATED)
+        return ResponseData.Response(TYPECODE.SI, TYPECODE.BAD_REQUEST, MESSAGE.BAD_REQUEST, serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        if self.get_queryset(pk):
+            objective_serializer = self.serializer_class(
+                self.get_queryset(pk), data=request.data)
+            if objective_serializer.is_valid():
+                objective_serializer.save()
+                return ResponseData.Response(TYPECODE.NO, TYPECODE.OK, MESSAGE.UPDATE, MESSAGE.NULL, status.HTTP_200_OK)
+            return ResponseData.Response(TYPECODE.SI, TYPECODE.BAD_REQUEST, MESSAGE.BAD_REQUEST, objective_serializer.errors, status.HTTP_400_BAD_REQUEST)
+        return ResponseData.Response(TYPECODE.SI, TYPECODE.NOT_FOUND, MESSAGE.NOT_FOUND, MESSAGE.NULL, status.HTTP_404_NOT_FOUND)
+
+    def destroy(self, request, pk=None):
+        try:
+            objective_serializer = self.get_queryset().filter(id=pk).first()
+            
+            if objective_serializer:
+                
+                objective_serializer.state = 'I'
+                objective_serializer.id_user_modified = request.data['id_user_modified']
+                objective_serializer.save()
+                return ResponseData.Response(TYPECODE.NO, TYPECODE.OK, MESSAGE.DESTROY, MESSAGE.NULL, status.HTTP_200_OK)
+            return ResponseData.Response(TYPECODE.SI, TYPECODE.NOT_FOUND, MESSAGE.NOT_FOUND, MESSAGE.NULL, status.HTTP_404_NOT_FOUND)
+        except:
+            return ResponseData.Response(TYPECODE.SI, TYPECODE.INTERNAL_ERROR, MESSAGE.INTERNAL_ERROR, MESSAGE.NULL, status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def partial_update(self, request, pk=None):
+        if self.get_queryset(pk):
+            objective_serializer = self.serializer_class(
+                self.get_queryset(pk), data=request.data)
+            if objective_serializer.is_valid():
+                objective_serializer.save()
+                return ResponseData.Response(TYPECODE.NO, TYPECODE.OK, MESSAGE.UPDATE, MESSAGE.NULL, status.HTTP_200_OK)
+            return ResponseData.Response(TYPECODE.SI, TYPECODE.BAD_REQUEST, MESSAGE.BAD_REQUEST, objective_serializer.errors, status.HTTP_400_BAD_REQUEST)
+        return ResponseData.Response(TYPECODE.SI, TYPECODE.NOT_FOUND, MESSAGE.NOT_FOUND, MESSAGE.NULL, status.HTTP_404_NOT_FOUND)
